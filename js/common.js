4fcfb1511c29432436b1312bb68b25aba55a96fb
nj-bay <neal.jacob.415@gmail.com>
2014-02-08 13:28:06 -0800

    fixed flip cards, buttonon/off, changes visual dimensions of Flower images

--- js/common.js
+++ js/common.js
@@ -1,16 +1,34 @@
+
 function animationClick(element, animation){
-	element_byId = document.getElementById(element);
-	populateDiv(element_byId);
-	element_byId.style.display = "block";
-    element = $("#"+element);
+	if ($('.container #pageUpContent').length )
+	{
+		if( element_byId.style.display == "none")
+		{
+			element_byId.style.display = "block";
+		}
+		else
+		{
+			element_byId.style.display = "none";
+		}
+		/*$('.container #pageUpContent').remove;*/
+	}
+	else
+	{
+		element_byId = document.getElementById(element);
+		populateDiv(element_byId);
+		element_byId.style.display = "block";
+		element = $("#"+element);
+		    element.addClass('animated ' + animation);         
+		    //wait for animation to finish before removing classes
+		    window.setTimeout( function(){
+		        element.removeClass('animated ' + animation);
+		    }, 2000);     
+		$('.container').append(element);
+	}
 
-            element.addClass('animated ' + animation);         
-            //wait for animation to finish before removing classes
-            window.setTimeout( function(){
-                element.removeClass('animated ' + animation);
-            }, 2000);          
-
+    
 }
+
 
 var flower_map_front = new Array();
 flower_map_front[0] = "images/flowerpower/alstroemeria.jpg";
@@ -18,6 +36,8 @@ flower_map_front[1] = "images/flowerpower/anthurium.jpg";
 flower_map_front[2] = "images/flowerpower/aster.jpg";
 flower_map_front[3] = "images/flowerpower/aster.jpg";
 flower_map_front[4] = "images/flowerpower/BirdsOfParadise.jpg";
+flower_map_front[5] = "images/flowerpower/BirdsOfParadise.jpg";
+flower_map_front[6] = "images/flowerpower/BirdsOfParadise.jpg";
 
 var flower_map_back = new Array();
 flower_map_back[0] = "images/flowerpower/Calla.jpg";
@@ -25,8 +45,11 @@ flower_map_back[1] = "images/flowerpower/freesia.jpg";
 flower_map_back[2] = "images/flowerpower/gerbera.jpg";
 flower_map_back[3] = "images/flowerpower/heather.jpg";
 flower_map_back[4] = "images/flowerpower/lily.jpg";
+flower_map_back[5] = "images/flowerpower/lily.jpg";
+flower_map_back[6] = "images/flowerpower/lily.jpg";
 
 var flip_template = "<div class=\"flip-container\" ontouchstart=\"this.classList.toggle('hover');\" class=\"image_tile\"> <div class=\"flipper\"> <div class=\"front\"><img src='<!-- front content -->'/></div> <div class=\"back\"> <img src='<!-- back content -->'/> </div></div></div>";
+
 
 function populateDiv(element) {
 	for (i = 0; i < flower_map_front.length; i++) {
