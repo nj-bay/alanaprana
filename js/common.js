8fcfc9ec6ac5f89702271279a4b6da2b380fc9a3
nj-bay <neal.jacob.415@gmail.com>
2014-02-13 21:07:20 -0800

    finishing touches

--- js/common.js
+++ js/common.js
@@ -1,10 +1,17 @@
 
 function animationClick(element, animation){
-	if ($('.container #pageUpContent').length )
+	if ($('.container #pageUpContent').length)
 	{
 		if( element_byId.style.display == "none")
 		{
 			element_byId.style.display = "block";
+			element = $("#"+element);
+		    element.addClass('animated ' + animation);         
+		    //wait for animation to finish before removing classes
+		    window.setTimeout( function(){
+		        element.removeClass('animated ' + animation);
+		    }, 2000);     
+		
 		}
 		else
 		{
@@ -31,24 +38,63 @@ function animationClick(element, animation){
 
 
 var flower_map_front = new Array();
-flower_map_front[0] = "images/flowerpower/alstroemeria.jpg";
-flower_map_front[1] = "images/flowerpower/anthurium.jpg";
-flower_map_front[2] = "images/flowerpower/aster.jpg";
-flower_map_front[3] = "images/flowerpower/sunflower.jpg";
-flower_map_front[4] = "images/flowerpower/Bouvardia.jpg";
-flower_map_front[5] = "images/flowerpower/Calla.jpg";
-flower_map_front[6] = "images/flowerpower/carnation.jpg";
+flower_map_front.push("images/flowerpower/alstroemeria.jpg");
+flower_map_front.push("images/flowerpower/anthurium.jpg");
+flower_map_front.push("images/flowerpower/aster.jpg");
+flower_map_front.push("images/flowerpower/sunflower.jpg");
+flower_map_front.push("images/flowerpower/Bouvardia.jpg");
+flower_map_front.push("images/flowerpower/Calla.jpg");
+flower_map_front.push("images/flowerpower/carnation.jpg");
+//flower_map_front[7] = "images/flowerpower/chrysanthemum.jpg";
+flower_map_front.push("images/flowerpower/daffodil.jpg");
+flower_map_front.push("images/flowerpower/delphinium.jpg");
+flower_map_front.push( "images/flowerpower/freesia.jpg");
+flower_map_front.push("images/flowerpower/gerbera.jpg");
+//flower_map_front[12] = "images/flowerpower/glysophilia.jpg";
+flower_map_front.push("images/flowerpower/heather.jpg");
+flower_map_front.push("images/flowerpower/hydrangea.jpg");
+flower_map_front.push("images/flowerpower/Iris.jpg");
+flower_map_front.push("images/flowerpower/liatris.jpg");
+//flower_map_front[17] = "images/flowerpower/lily.jpg";
+//flower_map_front[18] = "images/flowerpower/lilyOfTheValley.jpg";
+flower_map_front.push("images/flowerpower/lisianthus.jpg");
+//flower_map_front[20] = "images/flowerpower/orchid.jpg";
+flower_map_front.push("images/flowerpower/rose.jpg");
+flower_map_front.push("images/flowerpower/snapdragon.jpg");
+flower_map_front.push("images/flowerpower/statice.jpg");
+flower_map_front.push("images/flowerpower/sunflower.jpg");
+flower_map_front.push("images/flowerpower/tulip.jpg");
+
 
 var flower_map_back = new Array();
-flower_map_back[0] = "alstroemeria";
-flower_map_back[1] = "anthurium";
-flower_map_back[2] = "aster";
-flower_map_back[3] = "sunflower";
-flower_map_back[4] = "Bouvardia";
-flower_map_back[5] = "Calla";
-flower_map_back[6] = "carnation";
+flower_map_back.push("alstroemeria");
+flower_map_back.push("anthurium");
+flower_map_back.push("aster");
+flower_map_back.push("sunflower");
+flower_map_back.push("Bouvardia");
+flower_map_back.push("Calla");
+flower_map_back.push("carnation");
+//flower_map_back[7] = "chrysanthemum";
+flower_map_back.push("daffodil");
+flower_map_back.push("delphinium");
+flower_map_back.push("freesia");
+flower_map_back.push("gerbera");
+//flower_map_back[12] = "glysophilia";
+flower_map_back.push("heather");
+flower_map_back.push("hydrangea");
+flower_map_back.push("Iris");
+flower_map_back.push("liatris");
+//flower_map_back[17] = "lily";
+//flower_map_back[18] = "lilyOfTheValley";
+flower_map_back.push("lisianthus");
+//flower_map_back[20] = "orchid";
+flower_map_back.push("rose");
+flower_map_back.push("snapdragon");
+flower_map_back.push("statice");
+flower_map_back.push("sunflower");
+flower_map_back.push("tulip");
 
-var flip_template = "<div class=\"col-xs-6 col-md-4 flip-container\" ontouchstart=\"this.classList.toggle('hover');\" class=\"image_tile\"> <div class=\"flipper\"> <div class=\"front\"><img src='<!-- front content -->'/></div> <div class=\"back\"> <div id='back_content'><h3>{flower_map_back}</h3><p>{flower_back_content}</p><a href='#' onclick=\"displaymodal('{flower_map_back}')\">Read more</a> </div></div></div></div>";
+var flip_template = "<div class=\"col-xs-6 col-md-4 flip-container\" ontouchstart=\"this.classList.toggle('hover');\" class=\"image_tile\"> <div class=\"flipper\"> <div class=\"front\"><img class=\"img-rounded img-responsive\" src='<!-- front content -->'/></div> <div class=\"back\"> <div id='back_content'><h3>{flower_map_back}</h3><p>{flower_back_content}</p><a href='#' onclick=\"displaymodal('{flower_map_back}')\">Read more</a> </div></div></div></div>";
 var row_template = "<div class=\"row\">";
 //var wiki_url_template = "http://en.wikipedia.org/w/api.php?format=json&action=query&titles={title}&prop=revisions&rvprop=content"
 
